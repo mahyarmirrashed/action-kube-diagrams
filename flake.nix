fb723ece60054eacdc8a3b5996abe28d7da59fcb
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-04-18 18:01:55 -0700

    feat: add flake to build docker image

--- flake.nix
+++ flake.nix
@@ -3,20 +3,56 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
-    devShells.aarch64-darwin.default = let
-      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-    in
-      pkgs.mkShell {
-        packages = with pkgs; [
-          git
-          lazygit
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pythonEnv = pkgs.python312.withPackages (ps: [ ps.pyyaml ps.diagrams ]);
 
-          nodejs_20
-          nodePackages.prettier
-        ];
-      };
-  };
+        kube-diagrams = pkgs.stdenv.mkDerivation rec {
+          pname = "kube-diagrams";
+          version = "0.2.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "philippemerle";
+            repo = "KubeDiagrams";
+            rev = "v${version}";
+            hash = "sha256-mF0zL8D8bJ8BoaG43NLBwa+gwK6tHdiL+nGdtRfHj8Q=";
+          };
+          postPatch = ''
+            substituteInPlace bin/kube-diagrams \
+              --replace '/usr/bin/env python3' '${pythonEnv}/bin/python'
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp -r bin/* $out/bin/
+            chmod +x $out/bin/*-diagrams
+          '';
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            git
+            lazygit
+            nodePackages.prettier
+          ];
+        };
+
+        packages.default = pkgs.dockerTools.buildLayeredImage {
+          name = "ghcr.io/mahyarmirrashed/kube-diagrams";
+          tag = "latest";
+          contents = with pkgs; [
+            busybox
+            cacert
+            graphviz
+            kubernetes-helm
+            kube-diagrams
+            pythonEnv
+          ];
+          created = "now";
+        };
+      }
+    );
 }
